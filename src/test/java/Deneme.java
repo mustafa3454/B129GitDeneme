6f6231fc848983d448dda20ecc08703e681879fd
Ceren Ciftci <ciftcielifceren@gmail.com>
2023-02-25 18:48:31 +0300

    v1

---
+++ src/test/java/Deneme.java
@@ -0,0 +1,8 @@
+public class Deneme {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+        System.out.println("Dünya");
+    }
+}
+//Restore --> Tamamen Working Space'i temizler.
+//Stash --> Hafızaya alır.
